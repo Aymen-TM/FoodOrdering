5c841b6a81da6a132837b0abbe00a5a866cced21
Aymen-TM <t_mustapha_aymen@outlook.com>
2023-01-12 21:27:20 +0100

    add NavBar

---
+++ components/NavBar.js
@@ -0,0 +1,53 @@
+import { Badge, Box, Button, IconButton, Stack, Typography, useMediaQuery, useTheme } from '@mui/material'
+import Image from 'next/image'
+import React from 'react'
+import {logo, telephone} from '../public/img/index'
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Container } from '@mui/system';
+
+const NavBar = () => {
+  const isNotMobileScreen = useMediaQuery("(min-width:1000px)")
+  const theme = useTheme()
+  return (
+       <Container maxWidth="xl" sx={{
+        display:"flex",
+        alignItems:"center",
+        padding:"0 50px",
+        height:"100px",
+        bgcolor:theme.palette.primary.main
+       }}>
+      <Box display={"flex"}  alignItems={"center"} flex={{md:1}} gap={2}>
+        <Box display={"flex"} justifyContent={"center"} alignItems={"center"} height={48} width={48} borderRadius={"999999px"} bgcolor={theme.palette.background.default}>
+          <Image src={telephone} height={"32"} width={"32"} />
+        </Box>
+        <Stack gap={0.5}>
+          <Typography variant='p' fontWeight={"semibold"} fontSize={"15px"} color={"white"} textTransform={"uppercase"}>order now!</Typography>
+          <Typography  variant='h4'color={"white"} fontWeight={"bold"} textTransform={"uppercase"}>07 91 70 73 00</Typography>
+        </Stack>
+      </Box>
+      {isNotMobileScreen &&
+      <Stack flex={1} flexDirection={"row"} alignItems={"center"}  >
+        <Button  color={"secondary"} variant="text"><Typography fontWeight={"bold"} variant='h6'>Home</Typography></Button>
+        <Button color={"secondary"} variant="text"><Typography fontWeight={"bold"} variant='h6'>Products</Typography></Button>
+        <Button color={"secondary"} variant="text"><Typography fontWeight={"bold"} variant='h6'>Menu</Typography></Button>
+          <Box>
+            <Image src={logo} height={72} />
+          </Box>
+        <Button color={"secondary"} variant="text"><Typography fontWeight={"bold"} variant='h6'>Events</Typography></Button>
+        <Button color={"secondary"} variant="text"><Typography fontWeight={"bold"} variant='h6'>Blog</Typography></Button>
+        <Button color={"secondary"} variant="text"><Typography fontWeight={"bold"} variant='h6'>Contact</Typography></Button>
+      </Stack>
+      }
+
+      <Box display={"flex"} flex={1}  justifyContent={"end"}>
+        <IconButton >
+          <Badge badgeContent={<Typography color={theme.palette.primary.main} fontWeight={"bold"} variant='h6'>4</Typography>} variant="standard" color='secondary'  overlap="circular">
+            <ShoppingCartOutlinedIcon sx={{height:40,width:40}} color='secondary'  />
+          </Badge>
+        </IconButton>
+      </Box>
+      </Container>
+  )
+}
+
+export default NavBar
---
+++ public/img/index.js
@@ -0,0 +1,29 @@
+import arrowL from '../img/arrowl.png'
+import arrowR from '../img/arrowr.png'
+import bake from '../img/bake.png'
+import bg from '../img/bg.png'
+import bike from '../img/bike.png'
+import cart from '../img/cart.png'
+import checked from '../img/checked.png'
+import delivered from '../img/delivered.png'
+import logo from '../img/logo.png'
+import paid from '../img/paid.png'
+import pizza from '../img/pizza.png'
+import size from '../img/size.png'
+import telephone from '../img/telephone.png'
+
+
+export {
+    arrowL,
+    arrowR,
+    pizza,
+    bike,
+    bake,
+    size,
+    telephone,
+    delivered,
+    paid,
+    logo,
+    bg,cart,checked
+}
+
